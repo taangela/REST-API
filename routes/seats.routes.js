0c0662f966b9e90985da8febdcace5e2ab88d0ff
Marta Koperek <makoperek@gmail.com>
2020-02-18 00:10:15 +0100

    Add loadSeats function and interval

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db.js');
+const uuidv4 = require('uuid/v4');
 
 router.route('/seats').get((req, res) => {
   res.json(db.seats);
@@ -11,7 +12,10 @@ router.route('/seats/:id').get((req, res) => {
 });
 
 router.route('/seats').post((req, res) => {
-  res.json({ message: 'ok' });
+  const { day, seat, client, email } = req.body;
+  const newPost = {seat: seat, client: client, email: email, day: day, id: uuidv4()};
+  db.seats.push(newPost);
+  res.json({ message: 'OK' });
 });
 
 router.route('/seats:id').delete((req, res) => {
