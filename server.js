bc72cb6f4b524a627af931f5d7816fd8633ebaea
Marta Koperek <makoperek@gmail.com>
2020-02-17 23:28:31 +0100

    Add front

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-//const cors = require('express-cors');
-//const uuidv4 = require('uuid/v4');
+const cors = require('cors');
 
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -12,6 +11,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
@@ -22,6 +22,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 })
 
-app.listen(9000, () => {
-  console.log('Server is running on port: 9000');
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
