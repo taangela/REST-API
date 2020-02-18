fd3843044c1feef16f2c88d83612aebd7d12fe53
Marta Koperek <makoperek@gmail.com>
2020-02-18 21:17:50 +0100

    Prepare api to public in heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -9,6 +10,8 @@ const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname + '/client/build')));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors())
@@ -17,11 +20,14 @@ app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
