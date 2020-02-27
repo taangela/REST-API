41ce7808ca2d4c2f6828f13e41bb4724aa12f032
Marta Koperek <makoperek@gmail.com>
2020-02-27 23:22:21 +0100

    CHsnge endpoints in concerts routes

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,26 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db.js');
+const Concerts = require('../models/Concerts.model');
 
 
-router.route('/concerts').get((req, res) => {
-  res.json(db.concerts);
+router.get('/concerts', async (req, res) => {
+  try {
+    res.json( await Concerts.find());
+  }
+  catch(err) {
+    res.status(500).json(err)
+  }
 });
 
-router.route('/concerts/:id').get((req, res) => {
-  res.json(db.concerts[req.params.id - 1]);
+router.get('/concerts/:id', async (req, res) => {
+  try {
+    const dep = await Concerts.findById(req.params.id);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.route('/concerts').post((req, res) => {
-  res.json({ message: 'ok' });
+
+router.post('/concerts', async (req, res) => {
+  try {
+    const { performer, genre, price, day, image } = req.body;
+    const newConcerts = new Concerts({ performer: performer, genre: genre, price: price, day: day, image: image });
+    await newConcerts.save();
+    res.json({ message: 'OK' });
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.route('/concerts/:id').delete((req, res) => {
-  res.json({ message: 'ok' });
+router.put('/concerts/:id', async (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  try {
+    const dep = await(Concerts.findById(req.params.id));
+    if(dep) {
+      await Concerts.updateOne({ _id: req.params.id }, { $set: { performer: performer, genre: genre, price: price, day: day, image: image }});
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.route('/concerts/:id').put((req, res) => {
-  res.json({ message: 'ok' });
+router.delete('/concerts/:id', async (req, res) => {
+  try {
+    const dep = await(Concerts.findById(req.params.id));
+    if(dep) {
+      await Concerts.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
